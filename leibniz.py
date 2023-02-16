75fc5f3b8fbce3d90d088c82f8cf4e1c51909a75
generali.collins <104697851+generali-collins@users.noreply.github.com>
2023-02-16 21:06:54 +0000

    Add files via upload

---
+++ leibniz.py
@@ -0,0 +1,14 @@
+def leibniz ( terms):
+    acc = 0
+    num = 4
+    den = 1
+    for aterm in range (terms):
+        nexterm = num/den*(-1)**aterm
+        acc = acc +nexterm
+        den = den + 2
+    return acc
+
+leibniz = leibniz(15)
+print(leibniz)
+
+
